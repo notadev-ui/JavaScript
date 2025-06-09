26b0af98dbd2f585c0b1ef3afdce92a6e4986273
notadev-ui <noteadeveloper@gmail.com>
2025-06-09 23:56:09 +0530

    Looping

---
+++ basics/Looping.js
@@ -0,0 +1,45 @@
+//for loop example
+
+const results = document.querySelector("#results");
+
+function calculate() {
+  for (let i = 1; i < 10; i++) {
+    const newResult = `${i} x ${i} = ${i * i}`;
+    results.textContent += `${newResult}\n`;
+  }
+  results.textContent += "\nFinished!\n\n";
+}
+
+const calculateBtn = document.querySelector("#calculate");
+const clearBtn = document.querySelector("#clear");
+
+calculateBtn.addEventListener("click", calculate);
+clearBtn.addEventListener("click", () => (results.textContent = ""));
+
+
+initializer
+while (condition) {
+  // code to run
+
+  final-expression
+}
+// Example of a while loop
+
+const cats = ["Pete", "Biggles", "Jasmine"];
+
+let myFavoriteCats = "My cats are called ";
+
+let i = 0;
+
+while (i < cats.length) {
+  if (i === cats.length - 1) {
+    myFavoriteCats += `and ${cats[i]}.`;
+  } else {
+    myFavoriteCats += `${cats[i]}, `;
+  }
+
+  i++;
+}
+
+console.log(myFavoriteCats); // "My cats are called Pete, Biggles, and Jasmine."
+
