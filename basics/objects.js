f2f1a41053a486641e0b7aef97c1e82b7e107879
notadev-ui <noteadeveloper@gmail.com>
2025-06-09 23:57:35 +0530

    objects

--- basics/objects.js
+++ basics/objects.js
@@ -2,13 +2,18 @@
 const mySym = Symbol("Key1");
 
 const User = {
-    name: "John Doe",
-    age: 30,
+    name: "Nitish Singh",
+    age: 21,
     email: "xyz@gmail.com",
     location: "Basti",
     isloggedIn: false,
     [mySym]: "myKey1"
 };
 //console.log(`User Name is ${User["name"]}`); // This will throw an error because 'name' is not defined
-console.log( typeof User[mySym]);                         
-console.log(User)
+// console.log( typeof User[mySym]);                         
+// console.log(User)
+
+User.greeting = function() {
+    console.log(`Hello ${this.name}, welcome to the system!` );
+};
+User.greeting(); 
