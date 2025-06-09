20bf34b333cbfa406c24d01ad2917188f19bc71d
notadev-ui <noteadeveloper@gmail.com>
2025-06-09 23:48:37 +0530

    functions

---
+++ basics/Functions.js
@@ -0,0 +1,25 @@
+function myFunc(theArr) {
+  theArr[0] = 30;
+}
+
+const arr = [45];
+
+console.log(arr[0]); // 45
+myFunc(arr);
+console.log(arr[0]); // 30
+
+
+function map(f, a) {
+  const result = new Array(a.length);
+  for (let i = 0; i < a.length; i++) {
+    result[i] = f(a[i]);
+  }
+  return result;
+}
+
+const numbers = [0, 1, 2, 5, 10];
+const cubedNumbers = map(function (x) {
+  return x * x * x;
+}, numbers);
+console.log(cubedNumbers); // [0, 1, 8, 125, 1000]
+
